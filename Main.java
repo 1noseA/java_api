993e267618a599aaf3decd6f374c5b6bc4fc5c86
1noseA <1noseA2L@gmail.com>
2020-08-13 14:16:16 +0900

    [15-4]正規表現

--- Main.java
+++ Main.java
@@ -1,12 +1,16 @@
 public class Main {
   public static void main(String[] args) {
-    // StringBuilderを用いた連結（+演算子より早い）
-    // +演算子はそのたびにnewするためJVMに大きな負担がかかり遅い
-    StringBuilder sb = new StringBuilder();
-    for (int i = 0; i < 10; i++) {
-      sb.append("Java");
+    String s = "abc,def:ghi";
+    String[] words = s.split("[,:]");
+    for (String w : words) {
+      System.out.print(w + "->");
     }
-    String s = sb.toString();
-    System.out.println(s);
+
+    String w = s.replaceAll("[beh]", "X");
+    System.out.println(w);
   }
+
+  // public boolean isValidPlayerName(String name) {
+  //   return name.matches("[A-Z][A-Z0-9]{7}");
+  // }
 }
