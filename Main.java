e97e08d18039f464311ea52d61b6b34a78d63f09
1noseA <1noseA2L@gmail.com>
2020-08-13 13:48:43 +0900

    [15-3]StringBuilder

--- Main.java
+++ Main.java
@@ -1,22 +1,12 @@
 public class Main {
   public static void main(String[] args) {
-    String s1 = "Java and JavaScript";
-    // 15-2-2 文字列を検索する
-    if (s1.contains("Java")) {
-      System.out.println("文字列s1は、Javaを含んでいます");
+    // StringBuilderを用いた連結（+演算子より早い）
+    // +演算子はそのたびにnewするためJVMに大きな負担がかかり遅い
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < 10; i++) {
+      sb.append("Java");
     }
-    if (s1.endsWith("Java")) {
-      System.out.println("文字列s1は、Javaが末尾にあります");
-    }
-    System.out.println("文字列s1で最初にJavaが登場する位置は" + s1.indexOf("Java")); // 0
-    System.out.println("文字列s1で最後にJavaが登場する位置は" + s1.lastIndexOf("Java")); // 9
-
-    // 15-2-3 文字を切り出す
-    String s2 = "Java programming";
-    // 先頭位置は0
-    System.out.println("文字列s2の４文字目以降は" + s2.substring(3));
-    // =>"a programming"
-    System.out.println("文字列s2の４〜８文字目は" + s2.substring(3, 8));
-    // =>"a pro"
+    String s = sb.toString();
+    System.out.println(s);
   }
 }
