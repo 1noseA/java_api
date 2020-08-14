aaa68364c1f2fa1f577a05a5229d6aec2ac90c23
1noseA <1noseA2L@gmail.com>
2020-08-14 11:04:41 +0900

    [16-1]ArrayList

--- Main.java
+++ Main.java
@@ -1,16 +1,13 @@
-import java.time.*;
+import java.util.ArrayList;
 
 public class Main {
   public static void main(String[] args) {
-    LocalDate d1 = LocalDate.of(2020, 1, 1);
-    LocalDate d2 = LocalDate.of(2020, 1, 4);
-
-    // ３日間を表すperiodを２通りの方法で生成
-    Period p1 = Period.ofDays(3);
-    Period p2 = Period.between(d1, d2);
-
-    // d2のさらに３日後を計算する
-    LocalDate d3 = d2.plus(p2);
-    System.out.println(d3);
+    ArrayList<Integer> points = new ArrayList<Integer>();
+    points.add(10);
+    points.add(80);
+    points.add(75);
+    for (int i : points) {
+      System.out.println(i);
+    }
   }
 }
