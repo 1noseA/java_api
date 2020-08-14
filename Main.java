3af2e97bf4921b95f06e5410eaa8051fdec04843
1noseA <1noseA2L@gmail.com>
2020-08-14 11:19:03 +0900

    [16-2]イテレーター

--- Main.java
+++ Main.java
@@ -1,13 +1,17 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class Main {
   public static void main(String[] args) {
-    ArrayList<Integer> points = new ArrayList<Integer>();
-    points.add(10);
-    points.add(80);
-    points.add(75);
-    for (int i : points) {
-      System.out.println(i);
+    ArrayList<String> names = new ArrayList<String>();
+    names.add("湊");
+    names.add("朝香");
+    names.add("菅原");
+    Iterator<String> it = names.iterator();
+    // 矢印を次に進められるなら繰り返す
+    while (it.hasNext()) {
+      // 矢印を次に進め、内容を取り出す
+      String e = it.next();
+      System.out.println(e);
     }
   }
 }
