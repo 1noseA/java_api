5cbe57cf34b7c60ad144b7ae62b3a569e2de0b88
1noseA <1noseA2L@gmail.com>
2020-08-14 15:45:55 +0900

    [17-7]throw

--- Main.java
+++ Main.java
@@ -2,11 +2,7 @@ import java.io.*;
 
 public class Main {
   public static void main(String[] args) {
-    // try-with-resources文を使えば自動的にcloseされる
-    try (FileWriter fw = new FileWriter("data.txt");) {
-      fw.write("hello!");
-    } catch (Exception e) {
-      System.out.println("何らかの例外が発生しました");
-    }
+    Person p = new Person();
+    p.setAge(-128);
   }
 }
