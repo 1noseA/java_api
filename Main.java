101ae3dffaf0864e4d48dfd69b1044fd4b788a62
1noseA <1noseA2L@gmail.com>
2020-08-14 11:42:33 +0900

    [16-4-2]HashSet

--- Main.java
+++ Main.java
@@ -1,17 +1,17 @@
-import java.util.*;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
   public static void main(String[] args) {
-    ArrayList<String> names = new ArrayList<String>();
-    names.add("湊");
-    names.add("朝香");
-    names.add("菅原");
-    Iterator<String> it = names.iterator();
-    // 矢印を次に進められるなら繰り返す
-    while (it.hasNext()) {
-      // 矢印を次に進め、内容を取り出す
-      String e = it.next();
-      System.out.println(e);
+    Set<String> colors = new HashSet<String>();
+    colors.add("赤");
+    colors.add("青");
+    colors.add("黄");
+    // colors.add("赤");
+    // System.out.println("色は" + colors.size() + "種類"); // 3種類になる
+
+    for (String s : colors) {
+      System.out.print(s + "→");
     }
   }
 }
