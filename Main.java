2b33c296c504fa0d10d07db4fa4b6c3b6717d3a2
1noseA <1noseA2L@gmail.com>
2020-08-14 16:21:17 +0900

    [18-1]FileWriter

--- Main.java
+++ Main.java
@@ -3,14 +3,13 @@ import java.io.*;
 public class Main {
   public static void main(String[] args) throws Exception {
     // ファイルを開く
-    FileReader fr = new FileReader("data.txt");
-    int input = fr.read();
-    // ファイルの最後まで一文字ずつ読む
-    while (input != -1) {
-      System.out.println((char)input);
-      input = fr.read();
-    }
+    FileWriter fw = new FileWriter("data.txt");
+    // 一文字ずつ書く
+    fw.write('そ');
+    fw.write('れ');
+    fw.write('で');
+    fw.write('は');
     // ファイルを閉じる
-    fr.close();
+    fw.close();
   }
 }
