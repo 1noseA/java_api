48abf314ae079104d984e600168e83c4460466b0
1noseA <1noseA2L@gmail.com>
2020-08-14 15:22:38 +0900

    [17-5]try-with-resources

--- Main.java
+++ Main.java
@@ -2,10 +2,11 @@ import java.io.*;
 
 public class Main {
   public static void main(String[] args) {
-    try {
-      FileWriter fw = new FileWriter("data.txt");
-    } catch (IOException e) {
-      System.out.println("エラーが発生しました");
+    // try-with-resources文を使えば自動的にcloseされる
+    try (FileWriter fw = new FileWriter("data.txt");) {
+      fw.write("hello!");
+    } catch (Exception e) {
+      System.out.println("何らかの例外が発生しました");
     }
   }
 }
