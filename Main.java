b148479f872d45e2fcc5c503fd6b874c252998aa
1noseA <1noseA2L@gmail.com>
2020-08-14 12:47:31 +0900

    [17-3]チェック例外

--- Main.java
+++ Main.java
@@ -1,15 +1,11 @@
-import java.util.*;
+import java.io.*;
 
 public class Main {
   public static void main(String[] args) {
-    // 自然順序づけ（辞書順）
-    Map<String, Integer> prefs = new HashMap<String, Integer>();
-    prefs.put("京都府", 255);
-    prefs.put("東京都", 1261);
-    prefs.put("熊本県", 181);
-    for (String key : prefs.keySet()) {
-      int value = prefs.get(key);
-      System.out.println(key + "の人口は、" + value);
+    try {
+      FileWriter fw = new FileWriter("data.txt");
+    } catch (IOException e) {
+      System.out.println("エラーが発生しました");
     }
   }
 }
